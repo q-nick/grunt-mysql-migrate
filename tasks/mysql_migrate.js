71a448254e3c07b7e85a246c8e0c33477dc7ea64
Paweł <pawel.stefanski@locon.pl>
2014-03-14 12:37:55 +0100

    ''

--- tasks/mysql_migrate.js
+++ tasks/mysql_migrate.js
@@ -15,36 +15,8 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('mysql_migrate', 'Grunt plugin for mysql datasbe migrate through many databses', function() {
     // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+    var options = this.options();
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
+    console.log(options);
 
 };
